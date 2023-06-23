556b85b717946052b7a62913eaa30ec002ef284e
Andrey Belkin <dkvd1@yandex.ru>
2023-06-23 10:59:13 +0500

    add dockerfile and docker-compose

--- src/main/java/ru/netology/rest/advice/ExceptionHandlerAdvice.java
+++ src/main/java/ru/netology/rest/advice/ExceptionHandlerAdvice.java
@@ -1,11 +1,12 @@
 package ru.netology.rest.advice;
 
-import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.netology.rest.exception.InvalidCredentials;
 import ru.netology.rest.exception.UnauthorizedUser;
+
+import javax.validation.ConstraintViolationException;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
